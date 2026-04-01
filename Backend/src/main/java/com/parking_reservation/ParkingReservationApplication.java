a216574244bf72a788999a500117f368c8ac4a06
RUSH <159695212+UKRUSH@users.noreply.github.com>
2026-04-01 14:54:20 +0530

    first commit

---
+++ Backend/src/main/java/com/parking_reservation/ParkingReservationApplication.java
@@ -0,0 +1,13 @@
+package com.parking_reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingReservationApplication.class, args);
+	}
+
+}
